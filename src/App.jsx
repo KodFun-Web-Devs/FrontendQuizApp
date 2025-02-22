a0d7d65f72c66ab783f7e381122795d2739e3056
Sinan Kapusız <sinankapusiz@outlook.com>
2025-02-22 20:19:31 +0300

    index.html title değitirildi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [sayac, setSayac] = useState(3);
+  const [kullanici, setKullanici] = useState({ isim: "Ali", yas: 25 });
   return (
-    <div>
-      <h1>Hello, React with Vite!</h1>
-    </div>
+    
+  <div>
+    <h1>Sayaç : {sayac} </h1>
+    <button onClick={()=>setSayac(sayac+1)}>Sayaçı +1 Arttır.</button>
+  </div >
   );
 }
 
